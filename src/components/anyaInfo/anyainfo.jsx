26fb6276f291567fa1afaeb00dc4e4780ce07d4f
chuxs <42435519+chuxs@users.noreply.github.com>
2025-07-17 22:56:38 +0100

    initial commit

---
+++ src/components/anyaInfo/anyainfo.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './anyainfo.css';
+
+const mainPage = () => {
+  return (
+    <>
+    <header>
+        <nav>
+
+        </nav>
+    </header>
+        <section class="features" id="feat">
+            <div class="feature-grid">
+
+                <div class="feature-card">
+                    <i class="fa-solid fa-wand-sparkles"></i>
+                    <h3>AI Optimization</h3>
+                    <p>Let our AI help you tailor your resume to match job requirements perfectly</p>
+                </div>
+                <div class="feature-card">
+                    <i class="fa-solid fa-pencil"></i>
+                    <h3>Custom Generation</h3>
+                    <p>Create a tailored CV from scratch using AI assistance</p>
+                </div>
+
+            </div>
+        </section>
+        </>
+
+  );
+}
+
+export default mainPage;
+
+// const WeatherApp = () => { -----you can add more functionality here
